4977e0673b8d43d9441c102f496c225152dc75e6
Panyapat Wongdee <panyapat12345@gmail.com>
2022-02-18 16:43:43 +0700

    lab26_3.cpp completed

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
-//Write function count() here.
+int count(int d[], int n){
+	set<int> myset(d, d+n);
+	return myset.size();
+}
 
 int main(){	
-	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
+	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12};
 	
 	cout << "There are " << count(data,sizeof(data)/sizeof(int)); 
 	cout << " different numbers in data.";
